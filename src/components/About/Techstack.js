2ef53730130d6a4ea3c0388335693561de4c06ba
Divyang Surani <105037987+divyangsurani@users.noreply.github.com>
2024-05-14 12:48:33 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,15 +3,10 @@ import { Col, Row } from "react-bootstrap";
 import {
   DiJavascript1,
   DiReact,
-  DiNodejs,
   DiMongodb,
   DiGit,
   
 } from "react-icons/di";
-import {
-  SiNextdotjs,
-  SiExpress
-} from "react-icons/si";
 
 function Techstack() {
   return (
@@ -20,16 +15,7 @@ function Techstack() {
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />

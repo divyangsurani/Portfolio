17efa99d4325845eb689a4ac6e8cb3fe9b9c9020
Divyang Surani <105037987+divyangsurani@users.noreply.github.com>
2024-10-23 11:53:33 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,9 +11,9 @@ function AboutCard() {
           <ImPointRight /> Hi Everyone, I am <span className="purple">DIVYANG SURANI </span>
             from <span className="purple"> Ahmedabad, India.</span>
             <br />
-            <ImPointRight />I am working at ZION ELIRA IT SERVICE for front-end developer .
+            <ImPointRight />I am working at ZION ELIRA IT SERVICE as front-end developer ( intern ) .
             <br />
-            <ImPointRight /> I Have an experience of 6 Months in React Js at Techvault Support Service llp.
+            <ImPointRight /> I Have an experience of 6 Months in React Js at Techvault Support Service llp as ( intern ) .
             <br />
             <ImPointRight />I Pursuing BCA in (Silver Oak University).
             <br />
